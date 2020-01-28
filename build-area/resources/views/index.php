a9cafac6103a4240c08709114c5fe357693905b0
i386angel <geraldoahnert@gmail.com>
2020-01-27 22:55:54 -0300

    Build Area | Testando retorno de views

---
+++ build-area/resources/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Build Area</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<center><h2>Hello World!</h2></center>
+</body>
+</html>

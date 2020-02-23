44fce0f9a30e0ad90c27a5c4cc63e7b7c70214d1
i386angel <geraldoahert@gmail.com>
2020-02-23 15:49:21 -0300

    Alterando a visão Cursos.

---
+++ ddc/resources/views/info/cursos.blade.php
@@ -0,0 +1,28 @@
+<!--- Extends Layout--->
+@extends('layout.layout')
+
+
+<!--- Section Init --->
+@section('content')
+
+
+
+
+
+
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid jumbotronStyle">
+    <div class="container">
+        <h1 class="display-2">{{ config('app.name', 'app.name') }}</h1>
+        <p class="display-5 lead jumbotronGray text-roboto">Mesclando tecnologia aos estudos.</p>
+    </div>
+</div>  
+
+
+
+
+
+
+
+<!--- Section Finish --->
+@endsection
